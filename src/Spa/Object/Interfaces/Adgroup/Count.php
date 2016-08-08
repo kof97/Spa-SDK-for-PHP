392b33727d3e7dc185b0ee2b066df0846fc288da
kof97 <1048434786@qq.com>
2016-08-08 19:01:38 +0800

    review

--- src/Spa/Object/Interfaces/Adgroup/Count.php
+++ src/Spa/Object/Interfaces/Adgroup/Count.php
@@ -32,13 +32,11 @@ class Count {
      * Init .
      */
     public function __construct($spa, $mod, $act) {
-
         $this->spa = $spa;
 
         $this->method = 'GET';
 
         $this->endpoint = $mod . '/' . $act;
-
     }
 
     /**
@@ -48,8 +46,25 @@ class Count {
      * @param array $headers The request headers.
      * @return Response
      */
-    public function send($params = array(), $headers = array(), $access_token = null) {
+    public function send($params = array(), $headers = array()) {
+        $data = $this->fieldInfo();
 
+        FieldsDetector::validateField($params, $data);
+
+        $response = $this->spa->sendRequest($this->method, $this->endpoint, $params, $headers);
+
+        return $response;
+    }
+
+    /**
+     * Send a request with the user's token.
+     *
+     * @param array $params       The request params.
+     * @param array $headers      The request headers.
+     * @param array $access_token The user's access token.
+     * @return Response
+     */
+    public function sendWithAccessToken($params = array(), $headers = array(), $access_token = null) {
         $data = $this->fieldInfo();
 
         FieldsDetector::validateField($params, $data);
