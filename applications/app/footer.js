76f28320d6c9b5fd1fa3c378a97422398fb54aef
kof97 <1048434786@qq.com>
2017-03-06 10:32:36 +0800

    audio

--- applications/app/footer.js
+++ applications/app/footer.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react'
 
 export default class Footer extends Component {
+	constructor(props) {
+		super(props);
+
+		this.progress = null;
+
+		this.updatePorgress = this.updatePorgress.bind(this);
+		this.radioPlay = this.radioPlay.bind(this);
+	}
+
+	updatePorgress(audioPlayer) {
+		this.progress = setInterval(function () {
+			if (audioPlayer === null) {
+				audioPlayer = document.getElementById('audio-player');
+			}
+
+			if (audioPlayer === null || audioPlayer.paused === true) {
+				return;
+			}
+
+			console.log(audioPlayer.paused);
+		}, 1000);
+	}
+
+	radioPlay(e) {
+		let audioPlayer = document.getElementById('audio-player');
+		let obj = e.target;
+
+		switch (obj.className) {
+			case 'radio-play':
+				obj.className = 'radio-stop';
+
+				clearInterval(this.progress);
+				audioPlayer.pause();
+				break;
+
+			case 'radio-stop':
+				obj.className = 'radio-play';
+
+				this.updatePorgress(audioPlayer);
+				audioPlayer.play();
+				break;
+
+			default: return;
+		}
+	}
+
 	render() {
 		return (
 			<footer className="app-footer">
 				<div className="radio-player">
 					<div className="radio-pic left">
-						<img src="../pics/default_album.jpg" />
+						<a href="javascript:void(0)"><img src="../pics/default_album.jpg" /></a>
 					</div>
 					<div className="radio-info left">
 						<title>给未来的自己</title>
@@ -14,13 +60,12 @@ export default class Footer extends Component {
 					</div>
 					<div className="radio-controller right">
 						<a href="javascript:void(0)"><span className="radio-list"></span></a>
-						<a href="javascript:void(0)"><span className="radio-stop"></span></a>
+						<a href="javascript:void(0)"><span onClick={this.radioPlay} className="radio-stop"></span></a>
 						<a href="javascript:void(0)"><span className="radio-next"></span></a>
 					</div>
 				</div>
-				<div className="radio-progress">
-
-				</div>
+				<div id="radio-progress"></div>
+				<audio id="audio-player" src="http://m8.music.126.net/20170306093357/e385526f3bf1a8e09334de602709b69a/ymusic/14b1/e12d/933e/a25b20007f534181f024c3a7b6941f4e.mp3"/>
 			</footer>
 		)
 	}
